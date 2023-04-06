6552db21178c63a4bbdd059081fd95e4d315bed3
ozlem <ozlemakcicek83@gmail.com>
2023-04-06 07:44:00 +0200

    jkjk

---
+++ js_ass/style.js
@@ -0,0 +1,123 @@
+//! 1.Aufgabe
+
+// const note= +prompt("Notunuzu giriniz..:")
+
+// let score=0;
+// if(note>0 && note<=25){
+// console.log("note=FF");}
+//  else if(note>25 && note<=45){
+//  console.log("note=DD");}
+//  else if(note>65 && note<=75){
+//  console.log("note=CC");}
+//   else if(note>75 && note<=90){
+//  console.log("note=BA");}
+//   else if(note>90 && note<=100){
+//  console.log("note=AA");}
+//  else{
+//     alert("hatali giris")
+//  }
+
+
+
+//! 2.Aufgabe
+
+// const month=prompt("ay ismi giriniz..:");
+
+
+// switch (month) {
+//     case 'Januar':
+//        console.log('1');
+//    break;     
+
+//         case 'Februar':
+//   console.log('2');
+
+//   break;       
+//   case 'March':
+//    console.log('3');
+//  break;
+//         case 'April':
+//     console.log('4');
+//  break;
+//         case 'Mai':
+//  console.log('5');
+//  break;
+//         case 'June':
+//    console.log('6');
+//  break;
+//         case 'July':
+//    console.log('7');
+//  break;
+//         case 'August':
+//     console.log('8');
+//  break;
+//         case 'September':
+//      console.log('9');
+//  break;
+//         case 'October':
+//     console.log('10');
+//  break;
+//         case 'November':
+//      console.log('11');
+//  break;
+//         case 'Dezember':
+// console.log('12');
+
+        
+//         break;
+
+//     default:
+//         console.log("tanimlanmis degil");
+//         break;
+// }
+
+
+ //! 3.Aufgabe
+
+
+//  const num1= +prompt("1.sayiyi giriniz lutfen..:")
+//  const num2= +prompt("2. sayiyi giriniz lutfen..:")
+//  const num3= +prompt("3. sayiyi giriniz lutfen..:")
+//  const islem= prompt("+,* islemlerinden birini seciniz lutfen..:")
+
+
+//  let sonuc=0;
+//  if(num1>num2 && num1>num3){
+//  console.log("max.num is num1");}
+
+// else if(num2>num1 && num2>num3){
+// console.log("max.num is num2");}
+
+// else { alert("max.num is num3")
+// }
+
+//  if(num1<num2 && num1<num3){
+//  console.log("min.num is num1");}
+
+// else if(num2<num1 && num2<num3){
+// console.log("min.num is num2");}
+
+// else { alert("min.num is num3")
+
+// }
+
+
+//  if(islem == "+"){
+//  sonuc=num1+num2+num3;}
+
+//  else if(islem == "*"){
+//  sonuc=num1*num2*num3;}
+
+//  else{
+//     alert("hatali giris")
+//  }
+// console.log(sonuc);
+
+
+
+//! 4.Aufgabe
+
+// const num= +prompt("sayi giriniz lutfen..:")
+// num%2 == 0? console.log('cift'): console.log('tek');
+ 
+ 
