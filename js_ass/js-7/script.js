c240a9a64bbbd264f82c463e711a1f8edce27ecf
ozlem <ozlemakcicek83@gmail.com>
2023-05-04 01:11:34 +0200

    bdfbd

--- js_ass/js-7/script.js
+++ js_ass/js-7/script.js
@@ -1,23 +1,67 @@
-const veriGetir=()=>{
-    fetch("./tv-shows.json")
-    .then((res)=>{
-        if(!res.ok){
-            throw new Error("URL de hata var")
-        }
-        return res.json()
+
+//! Fetch()
+// const veriGetir=()=>{
+//     fetch("./tv-shows.json")
+//     .then((res)=>{
+//         if(!res.ok){
+//             throw new Error("URL de hata var")
+//         }
+//         return res.json()
     
-    }).then((veri)=>ekranaBastir(veri))
+//     }).then((veri)=>ekranaBastir(veri))
     
 
 
-}
-veriGetir()
+// }
+// veriGetir()
 
 
-const ekranaBastir=(veri)=>{
-    veri.forEach((eleman) => {
-        document.querySelector('.tvShowList').innerHTML += `
-    <div class="col-md-3"> 
+// const ekranaBastir=(veri)=>{
+//     veri.forEach((eleman) => {
+//         document.querySelector('.tvShowList').innerHTML += `
+//     <div class="col-md-3"> 
+//  <div class="card" >
+//    <img src=${eleman.show.image.medium} class="card-img-top">
+//    <div class="card-body">
+//      <h5 class="card-title">${eleman.show.name}</h5> 
+//       <a class= "btn btn-success"  href=${eleman.show.url}>Detaylar</a>
+//      <p> </p>
+//    </div>
+//  </div>
+//  </div> `;
+        
+
+//     });
+// }
+
+
+
+
+
+//!Async-await
+
+const veriGetir = async () => {
+  try {
+    const response = await fetch("./tv-shows.json");
+
+    if (!response.ok) {
+      throw new Error(`url de hata var ${response.status}`);
+    }
+    const veri=await response.json();
+
+    ekranaBastir(veri);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    console.log("her durumda calis");
+  }
+};
+
+veriGetir();
+
+const ekranaBastir = (data) => {
+  data.forEach((eleman) => {
+    document.querySelector('.tvShowList').innerHTML += `<div class="col-md-3"> 
  <div class="card" >
    <img src=${eleman.show.image.medium} class="card-img-top">
    <div class="card-body">
@@ -27,7 +71,5 @@ const ekranaBastir=(veri)=>{
    </div>
  </div>
  </div> `;
-        
-
-    });
-}
+  });
+};
